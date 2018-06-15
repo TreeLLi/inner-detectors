054526f0786bfa16df950528e666490ad081de38
Lin Li <linli.tree@gmail.com>
2018-06-15 16:40:11 +0100

    unittest for loading data and iou calculation

--- test/data_loader_test.py
+++ test/data_loader_test.py
@@ -4,27 +4,63 @@ abs_path = os.path.dirname(os.path.abspath(__file__))
 par_path = os.path.join(abs_path, "..")
 sys.path.append(par_path)
 
-from src.data_loader import BatchLoader, SOURCE, loadBinaryData, loadImages
+from src.config import PATH
+from utils.helper.anno_parser import parsePASCALPartAnno
+from utils.helper.file_manager import loadBinaryData, loadImage
+from utils.helper.data_loader import *
 
 class TestDataLoader(unittest.TestCase):
+
+    def test_init(self):
+        bl = BatchLoader()
+        print (bl.batch_size, bl.amount)
+        
+        self.assertTrue(bl.batch_size == 10)
+        self.assertTrue(bl.amount == 10103)
+        self.assertTrue(bl.data)
+        
+    def test_bool(self):
+        bl = BatchLoader(sources=[PASCAL])
+        self.assertTrue(bool(bl))
+    
+    def test_size(self):
+        bl = BatchLoader(sources=[PASCAL], amount=10)
+        self.assertTrue(bl.size == 10)
+        
+    def test_next(self):
+        bl = BatchLoader()
+        batch = bl.nextBatch()
+        self.assertTrue(batch)
+
+        self.assertTrue(len(batch.ids) == len(batch.imgs))
+        self.assertTrue(len(batch.annos) == 10)
+
+
+class TestFileManager(unittest.TestCase):
 
     def test_load_binary(self):
         data = loadBinaryData(os.path.join(par_path, "datasets/annos_10.pkl"))
         self.assertTrue(data)
 
-    def test_load_images(self):
+    def test_load_image(self):
         path = os.path.join(par_path, "datasets/VOC2010/JPEGImages")
-        images = loadImages(path, ["2008_004198.jpg"])
-        self.assertTrue(images)
+        image = loadImage(path, "2008_004198.jpg")
+        self.assertTrue(image)
         
-    def test_bool(self):
-        bl = BatchLoader(sources=[SOURCE.PASCAL])
-        self.assertTrue(bl)
+        
+class TestAnnoParser(unittest.TestCase):
 
-    def test_next(self):
-        bl = BatchLoader(sources=[SOURCE.PASCAL])
-        next = bl.next()
-        self.assertTrue(next)
-    
+    def test_parse_pascal_anno(self):
+        directory = PATH.DATA.PASCAL.ANNOS
+        file_name = "2008_004198.mat"
+        
+        annos, labels = parsePASCALPartAnno(directory, file_name)
+        self.assertTrue(len(annos) == 13)
+        self.assertTrue(len(labels) == 1)
+        self.assertTrue(annos[1].name == "head")
+        self.assertTrue(annos[0].category == "object")
+        self.assertTrue(annos[2].partof == annos[0].name)
+
+        self.assertTrue(labels[0] == 15)
 if __name__ == "__main__":
     unittest.main()
--- test/reflector_test.py
+++ test/reflector_test.py
@@ -1,10 +1,12 @@
 import unittest, os, sys
+import numpy as np
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 par_path = os.path.join(abs_path, "..")
 sys.path.append(par_path)
 
-from utils.dissection.iou import iou
+from utils.dissection.iou import iou, binary
+
 
 class TestIoU(unittest.TestCase):
 
@@ -12,7 +14,17 @@ class TestIoU(unittest.TestCase):
         mask1 = np.asarray([[0,0,0],[0,1,0],[0,0,0]])
         mask2 = np.asarray([[0,0,0],[1,1,1],[0,1,0]])
         i = iou(mask1, mask2)
+        
+        print ("IoU = {}".format(i))
         self.assertTrue(i == 0.25)
-    
+
+    def test_binary(self):
+        mask = np.asarray([[2,2], [-1,0]])
+        mask = binary(mask)
+
+        print (mask)
+        
+        self.assertTrue(mask[0][0] == 1)
+        
 if __name__ == "__main__":
     unittest.main()
