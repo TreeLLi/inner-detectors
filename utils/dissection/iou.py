3145bd9fb8824501d17eb8ca9d33eedd19d82562
Lin Li <linli.tree@gmail.com>
2018-06-15 16:43:20 +0100

    iou calculation

--- utils/dissection/iou.py
+++ utils/dissection/iou.py
@@ -5,10 +5,20 @@ To calculate pixel-wise IoU between two masks
 
 '''
 
+import numpy as np
 
 def iou(mask1, mask2, binary=True):
     if not binary:
-        mask1 = 
+        mask1 = binary(mask1)
+        mask2 = binary(mask2)
+
+    mask = mask1 + mask2
+    count_1 = np.sum(mask == 1)
+    count_2 = np.sum(mask == 2)
+
+    return float(count_2) / (count_1 + count_2)
 
 def binary(mask):
+    mask[mask>0] = 1
     
+    return mask
