9435bf460ff559f76051e42ae3e6716d19c7345b
Lin Li <linli.tree@gmail.com>
2018-07-01 21:40:44 +0100

    Test base - add assert functions

--- test/test_helper.py
+++ test/test_helper.py
@@ -7,7 +7,7 @@ via the form of Base Class
 '''
 
 import unittest
-
+import numpy as np
 
 class TestBase(unittest.TestCase):
 
@@ -16,3 +16,23 @@ class TestBase(unittest.TestCase):
         
         self.longMessage = True
         
+    def assertListEqual(self, first, second, msg=None):
+        first = first.tolist() if isinstance(first, np.ndarray) else first
+        second = second.tolist() if isinstance(second, np.ndarray) else second
+
+        return super(TestBase, self).assertListEqual(first, second, msg)
+
+    def assertEmpty(self, expr):
+        return self.assertLengthEqual(expr, 0)
+    
+    def assertNotEmpty(self, expr):
+        length = len(expr)
+        self.assertGreater(length, 0)
+
+    def assertLengthEqual(self, expr, length):
+        self.assertEqual(len(expr), length)
+
+    def assertShapeEqual(self, expr, shape):
+        if isinstance(expr, np.ndarray):
+            self.assertEqual(expr.shape, shape)
+        # TODO - shape comparison of primitive list
