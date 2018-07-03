2007feed7e2ca2b640354c7ec870d638c6ab399f
Lin Li <linli.tree@gmail.com>
2018-07-03 21:42:15 +0100

    TestBase - function info log, rearrange functions layout

--- test/test_helper.py
+++ test/test_helper.py
@@ -8,13 +8,37 @@ via the form of Base Class
 
 import unittest
 import numpy as np
+import inspect
+
+count = {}
 
 class TestBase(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestBase, self).__init__(*args, **kwargs)
-        
         self.longMessage = True
+
+    '''
+    Print
+
+    '''
+
+    def log(self):
+        stack = inspect.stack()
+        class_name = self.__class__.__name__
+        func_name = stack[1][3]
+        if class_name not in count:
+            count[class_name] = 1
+        else:
+            count[class_name] += 1
+        print ("{} - {}: {}".format(class_name,
+                                    count[class_name],
+                                    func_name))
+        
+    '''
+    Assertion
+    
+    '''
         
     def assertListEqual(self, first, second, msg=None):
         first = first.tolist() if isinstance(first, np.ndarray) else first
