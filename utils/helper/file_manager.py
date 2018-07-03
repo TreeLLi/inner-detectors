63fed733aa0aac28a4483b52693007814645516a
Lin Li <linli.tree@gmail.com>
2018-07-03 21:44:16 +0100

    File manager - load, save object

--- utils/helper/file_manager.py
+++ utils/helper/file_manager.py
@@ -7,6 +7,7 @@ To manage all files' inputs and outpus
 
 
 import os, sys
+import pickle
 from scipy.misc import imread
 
 
@@ -35,3 +36,19 @@ def loadListFromText(file_path):
     except:
         print ("Error: failed to load text file {}".format(file_path))
         return None
+
+def loadObject(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            obj = pickle.load(f)
+            return obj
+    except:
+        print ("Error: failed to load object from file {}".format(file_path))
+        return None
+
+def saveObject(obj, file_path):
+    try:
+        with open(file_path, 'wb+') as f:
+            pickle.dump(obj, f)
+    except:
+        print ("Error: failed to save object at {}".format(file_path))
