e8d5a860971907a93d540e1b3cb93fa23273142a
Lin Li <linli.tree@gmail.com>
2018-07-31 11:18:24 +0100

    Activ thres - save

--- src/activ_thres.py
+++ src/activ_thres.py
@@ -16,9 +16,10 @@ if root_path not in sys.path:
     sys.path.insert(0, root_path)
 
 from src.config import PATH, CONFIG
-from src.indr_matcher import matchActivsAnnos, combineMatches
+from src.indr_matcher import matchActivsAnnos, combineMatches, reportProgress
 
 from utils.helper.data_loader import BatchLoader
+from utils.helper.dstruct_helper import splitDict
 from utils.helper.file_manager import saveObject, loadObject
 from utils.model.model_agent import ModelAgent
 from utils.dissection.helper import iou
@@ -30,12 +31,14 @@ Multiprocessing
 
 '''
 
-def process(activ_maps, field_maps, annos, quan, matches, idx):
-    print ("Processing Quantile {}%".format(quan))
-    ref_activ_maps = reflect(activ_maps, field_maps, quan=quan)
-    batch_matches = matchActivsAnnos(ref_activ_maps, annos)
-    matches[idx] = combineMatches(matches[idx], batch_matches)
-    print ("Finished processing Quantile {}%".format(quan))
+def process(activ_maps, field_maps, annos, quans):
+    batch_matches = []
+    for quan in quans:
+        ref_activ_maps = reflect(activ_maps, field_maps, quan=quan)
+        batch_match = matchActivsAnnos(ref_activ_maps, annos)
+        batch_matches.append(batch_match)
+    print ("process finished.")
+    return batch_matches
 
     
 '''
@@ -44,7 +47,7 @@ Main Program
 '''
 
 if __name__ == "__main__":
-    bl = BatchLoader(amount=20, mode="classes")
+    bl = BatchLoader(amount=4000, mode="classes")
     probe_layers = loadObject(PATH.MODEL.PROBE)
     model = ModelAgent()
     field_maps = model.getFieldmaps()
@@ -54,13 +57,21 @@ if __name__ == "__main__":
     num = pool._processes
     matches = [None for x in range(len(quans))]
     while bl:
+        start = time.time()
         batch = bl.nextBatch()
         imgs = batch[1]
         annos = batch[2]
 
         activ_maps = model.getActivMaps(imgs, probe_layers)
-        params = []
-        for idx, quan in enumerate(quans):
-            params.append((activ_maps, field_maps, annos, quan, matches, idx))
-        pool.starmap(process, params)
+        activ_maps = splitDict(activ_maps, num)
+        params = [(amap, field_maps, annos, quans) for amap in activ_maps]
+        batch_matches = pool.starmap(process, params)
+        print ("Combine matches...")
+        for batch_match in batch_matches:
+            for idx, bm in enumerate(batch_match):
+                matches[idx] = combineMatches(matches[idx], bm)
+
+        reportProgress(start, time.time(), bl.batch_id, len(imgs), bl.size)
     # process results
+    path = os.path.join(PATH.OUT.ROOT, "vgg16/activ_thres.pkl")
+    saveObject(matches, path)
